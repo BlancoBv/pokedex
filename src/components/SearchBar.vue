a1f0a895c6b8e48b8b13a04179303c7ded28c998
blanco <dehara14@gmail.com>
2024-09-14 22:35:57 -0600

    correción de barra de busqueda

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -23,14 +23,19 @@ watch(value, async () => {
 <template>
 
     <div class="dropdown w-full">
-        <label class="input input-bordered flex items-center gap-2 w-full max-w-xs">
+
+        <label class="input input-bordered flex items-center w-48 md:w-auto overflow-clip">
             <input type="text" class="grow" placeholder="Filter by ID or name!" v-model="value" tabindex="1" />
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-4 w-4 opacity-70">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 opacity-70">
                 <path fill-rule="evenodd"
                     d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
                     clip-rule="evenodd" />
             </svg>
         </label>
+        <!--         <div class="form-control">
+            <input type="text" placeholder="filter by id or name!" class="input input-bordered w-24 md:w-auto"
+                v-model="value" />
+        </div> -->
         <ul class="dropdown-content menu bg-base-100 rounded-box w-full max-w-xs p-2 shadow z-50">
             <li v-if="pending" class="flex items-center">
                 <span class="loading loading-spinner loading-xs"></span>
