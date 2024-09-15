bce8bc8416b5121d151eedbc1ea17b1e63def21f
blanco <dehara14@gmail.com>
2024-09-15 11:43:13 -0600

    rutas estaticas

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,7 @@ import vue from "@astrojs/vue";
 
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [vue({ appEntrypoint: "/src/pages/_app" }), tailwind()],
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 4321,
-    host: true,
-  },
-  adapter: cloudflare(),
 });
